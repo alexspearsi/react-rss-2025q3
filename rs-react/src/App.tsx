6308afc08150173e33b6add98ec44e744959955d
Alex <strelchenko.aleksandr@yandex.ru>
2025-07-08 04:54:13 +0300

    feat: create spinner

--- rs-react/src/App.tsx
+++ rs-react/src/App.tsx
@@ -1,3 +1,4 @@
+import './App.css';
 import { Component } from 'react';
 import Main from './Main/Main';
 import Header from './Header/Header';
@@ -7,6 +8,7 @@ import type { Character } from './types/character';
 type AppState = {
   characters: Character[];
   searchQuery: string;
+  isLoading: boolean;
 };
 
 class App extends Component<object, AppState> {
@@ -15,19 +17,23 @@ class App extends Component<object, AppState> {
     this.state = {
       characters: [],
       searchQuery: '',
+      isLoading: false,
     };
   }
 
   async componentDidMount() {
-    const characters = [];
-    for (let i = 1; i <= 3; i++) {
+    this.setState({ isLoading: true });
+    const characters: Character[] = [];
+
+    for (let i = 1; i <= 24; i++) {
       const response = await fetch(
         `https://rickandmortyapi.com/api/character/?name=${this.state.searchQuery}&page=${i}`
       );
       const data = await response.json();
       characters.push(...data.results);
     }
-    this.setState({ characters: characters });
+
+    this.setState({ characters: characters, isLoading: false });
   }
 
   handleSearchChange = (value: string) => {
@@ -35,7 +41,7 @@ class App extends Component<object, AppState> {
   };
 
   render() {
-    const { characters, searchQuery } = this.state;
+    const { characters, searchQuery, isLoading } = this.state;
     const filtered = characters.filter((character) =>
       character.name.toLowerCase().includes(searchQuery.toLowerCase())
     );
@@ -44,9 +50,13 @@ class App extends Component<object, AppState> {
       <>
         <Header onSearchChange={this.handleSearchChange} />
         <Main>
-          {filtered.map((character, index) => (
-            <Card key={index} character={character} />
-          ))}
+          {isLoading ? (
+            <div className="spinner"></div>
+          ) : (
+            filtered.map((character, index) => (
+              <Card key={index} character={character} />
+            ))
+          )}
         </Main>
       </>
     );
--- rs-react/src/Main/Main.tsx
+++ rs-react/src/Main/Main.tsx
@@ -1,5 +1,5 @@
 import './Main.css';
-import { Component } from 'react';
+import { Component, Children } from 'react';
 
 type WithClassName = {
   children: React.ReactNode;
@@ -8,8 +8,19 @@ type WithClassName = {
 class Main extends Component<WithClassName> {
   render() {
     const { children } = this.props;
+    const childrenCount = Children.count(children);
 
-    return <main className="main">{children}</main>;
+    return (
+      <main className="main">
+        {childrenCount === 0 ? (
+          <p className="main__nothing-found">
+            Morty, we searched the multiverse... and found nothing
+          </p>
+        ) : (
+          children
+        )}
+      </main>
+    );
   }
 }
 
