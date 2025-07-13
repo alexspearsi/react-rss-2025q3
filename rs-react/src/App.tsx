a96193114c2227f94b78151b5704e5d79a7edf35
Alex <strelchenko.aleksandr@yandex.ru>
2025-07-14 00:29:31 +0300

    refactor: refactor code

--- rs-react/src/App.tsx
+++ rs-react/src/App.tsx
@@ -28,6 +28,10 @@ export class App extends Component<object, AppState> {
       const data = await response.json();
       this.setState({ characters: data.results || [], isLoading: false });
     } catch {
+
+
+
+      
       this.setState({ characters: [], isLoading: false });
     }
   }
