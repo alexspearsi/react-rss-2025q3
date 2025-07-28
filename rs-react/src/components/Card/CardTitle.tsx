f5ae6547af10059a6a90773c1162cbfad4402cf2
Alex <strelchenko.aleksandr@yandex.ru>
2025-07-28 07:03:47 +0300

    refactor: convert CardTitle from class to functional component

--- rs-react/src/components/Card/CardTitle.tsx
+++ rs-react/src/components/Card/CardTitle.tsx
@@ -1,12 +1,9 @@
 import './Card.css';
-import { Component } from 'react';
 
 type Props = {
   children: React.ReactNode;
 };
 
-export class CardTitle extends Component<Props> {
-  render() {
-    return <h2 className="card__title">{this.props.children}</h2>;
-  }
+export function CardTitle({ children }: Props) {
+  return <h2 className="card__title">{children}</h2>
 }
