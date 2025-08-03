08f2c3a93bacb91d17e3e30bdb848e7e57df069a
Alex <strelchenko.aleksandr@yandex.ru>
2025-08-03 23:24:55 +0300

    chore: install zustand library

--- rs-react/src/components/Card/Card.tsx
+++ rs-react/src/components/Card/Card.tsx
@@ -2,16 +2,24 @@ import styles from './Card.module.css';
 
 import { CardImage } from './CardImage';
 import { CardTitle } from './CardTitle';
-import type { Character } from '../../types/character';
+import { useCardStore } from '../../store/store'
 
 type Props = {
-  character: Character;
+  character: {
+    id: string;
+    image: string;
+    name: string;
+  };
   onClick?: () => void;
 };
 
-export function Card({ character: { image, name }, onClick }: Props) {
+export function Card({ character: { image, name, id }, onClick }: Props) {
   return (
     <div data-testid='card' className={styles.card} onClick={onClick}>
+      <input 
+        type="checkbox"
+        onClick={(e) => e.stopPropagation()}
+      />
       <CardImage image={image} name={name} />
 
       <CardTitle>{name}</CardTitle>
