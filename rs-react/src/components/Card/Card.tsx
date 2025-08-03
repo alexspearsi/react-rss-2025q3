61da137fd29c5d3716f0bfdf93d9e2ddb84c323a
Alex <strelchenko.aleksandr@yandex.ru>
2025-08-04 02:10:01 +0300

    feat: create zustand store

--- rs-react/src/components/Card/Card.tsx
+++ rs-react/src/components/Card/Card.tsx
@@ -6,7 +6,7 @@ import { useCardStore } from '../../store/store'
 
 type Props = {
   character: {
-    id: string;
+    id: number;
     image: string;
     name: string;
   };
@@ -14,11 +14,18 @@ type Props = {
 };
 
 export function Card({ character: { image, name, id }, onClick }: Props) {
+  const selectedItems = useCardStore((state) => state.selectedItems);
+  const toggleItem = useCardStore((state) => state.toggleItem);
+
+  const isChecked = selectedItems.includes(String(id));
+
   return (
     <div data-testid='card' className={styles.card} onClick={onClick}>
       <input 
         type="checkbox"
+        checked={isChecked}
         onClick={(e) => e.stopPropagation()}
+        onChange={() => toggleItem(String(id))}
       />
       <CardImage image={image} name={name} />
 
