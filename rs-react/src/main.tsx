b1b169b51ebd3ab252d5f9b162638404cb54070f
Alex <strelchenko.aleksandr@yandex.ru>
2025-08-18 15:53:39 +0300

    chore: setup ESLint, Prettier and Husky
    chore: setup lint-staged
    
    testing pre-commit code
    
    chore: update husky
    
    chore: update husky
    
    chore: add Husky to the project

---
+++ rs-react/src/main.tsx
@@ -0,0 +1,16 @@
+import './index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './app.tsx';
+
+const rootElement = document.getElementById('app');
+
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
